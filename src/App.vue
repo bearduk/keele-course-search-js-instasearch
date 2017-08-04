9aad717ff400227ab6ea5920e95456851eb9a972
Chris Beard <bearduk@users.noreply.github.com>
2017-08-04 14:29:11 +0100

    snippet added and a lot of new parameter settings added to the instansearch constructor.

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,22 @@
       <div id="search-box">
         <!-- SearchBox widget will appear here -->
       </div>
+      
+      <div id="courseLevel"></div>
+      <div id="clear-all"></div>
+
+      <div id="hits-per-page-selector"></div>
 
       <div id="hits">
         <!-- Hits widget will appear here -->
       </div>
+
+      <div id="pagination-container"></div>
   </div> <!-- .container -->
 </template>
 
 <script>
+// installed in main.js, let's use it here
 import instantsearch from 'instantsearch.js';
 
 export default {
@@ -26,23 +34,81 @@ export default {
           appId: 'HT7VYJG3KU',
           apiKey: 'd37bbf3291b226676c9f3f1937e865d3',
           indexName: 'dev_COURSES',
-          urlSync: true
+          urlSync: true,
+          searchParameters: {
+            hitsPerPage: 6,
+            attributesToSnippet: [
+            'snippet:25'
+            ],
+            snippetEllipsisText: '...'
+          }
         });
 
     // initialize SearchBox
     search.addWidget(
       instantsearch.widgets.searchBox({
         container: '#search-box',
-        placeholder: 'Search for products'
+        placeholder: 'Search for courses'
       })
     );
+
+    // refinement list
+    search.addWidget(
+      instantsearch.widgets.refinementList({
+          container: '#courseLevel',
+          attributeName: 'courseLevelName',
+          templates: {
+          header: 'Course Level',
+          sortBy: ["name:asc"]
+        }
+      })
+    );
+
+    // clear all 
+    search.addWidget(
+      instantsearch.widgets.clearAll({
+        container: '#clear-all',
+        templates: {
+          link: 'Clear filter'
+        },
+        autoHideContainer: true,
+        clearsQuery: false,
+    })
+  );
+
+    // hits per page
+    search.addWidget(
+      instantsearch.widgets.hitsPerPageSelector({
+        container: '#hits-per-page-selector',
+        items: [
+          {value: 10, label: '10 per page'},
+          {value: 20, label: '20 per page'},
+          {value: 30, label: '30 per page'},
+        ]
+      })
+    );    
 
     // initialize hits widget
     search.addWidget(
       instantsearch.widgets.hits({
-        container: '#hits'
+        container: '#hits',
+        templates: {
+        empty: 'No courses found',
+        item: '<div class="course-result"><!-- Hit {{ objectID }}: --><a href="{{ urlPath }}">{{{ _highlightResult.courseTitle.value }}}</a> <span class="course-level">{{courseLevel}}</span>\
+        <p>{{_snippetResult.snippet.value}}</p></div>'
+      }
       })
     );
+
+    search.addWidget(
+      instantsearch.widgets.pagination({
+        container: '#pagination-container',
+        maxPages: 20,
+        // default is to scroll to 'body', here we disable this behavior
+        scrollTo: false,
+        showFirstLast: false,
+    })
+  );
   
     search.start();
   }
@@ -50,30 +116,35 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
+    .ais-clear-all--link-disabled {
+      display: none;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+    /* throw some basic CSS in */
+    body {
+      font-family: sans-serif;
+    }
+    em {
+      font-weight: bold;
+      color: #FFA500;
+    }
+    .logo {
+      margin-bottom: 10px;
+    }
+    /* and for bootstrap nav */
+    body { padding-top: 70px; }
+      @media screen and (max-width: 768px) {
+          body { padding-top: 70px; }
+    }
+    .course-level {
+    border: 1px solid #e0e0e0;
+    padding: 3px 5px;
+    text-align: center;
+    text-transform: uppercase;
+    }
+    .course-result{
+      margin-bottom: 4px;
+    }
 </style>
